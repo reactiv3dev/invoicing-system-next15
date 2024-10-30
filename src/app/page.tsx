ed184c5a8880964ad85d5e0666bd571fed1af06c
reactiv3dev <reactivedeveloper@protonmail.com>
2024-10-30 05:21:03 +0100

    land page and docs changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center text-center h-screen gap-6 bg-inherit">
       <h1 className="text-7xl font-bold">InvoicIn</h1>
-      <p>
-        <Button className="bg-yellow-500" variant="ghost" asChild>
+      <div className="flex justify-center items-center gap-6">
+        <Button className="bg-yellow-500 p-6 w-1/4" variant="ghost" asChild>
           <Link href="/dashboard">
            Sign In
           </Link>
         </Button>
-        <Button className="bg-blue-500" variant="ghost" asChild>
+        <Button className="bg-blue-500 p-6 w-1/4" variant="ghost" asChild>
           <Link href="/docs">
           Documentation
           </Link>
         </Button>
-      </p>
+      </div>
     </main>
   );
 }
