f246b59b97335ddd0b21dcb2e4fe3055b3ce473f
reactiv3dev <reactivedeveloper@protonmail.com>
2024-10-30 05:42:42 +0100

    retry commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
     <main className="flex flex-col justify-center text-center h-screen gap-6 bg-inherit">
       <h1 className="text-7xl font-bold">InvoicIn</h1>
       <div className="flex justify-center items-center gap-6">
-        <Button className="bg-yellow-500 p-6 w-1/4" variant="ghost" asChild>
+        <Button className="bg-yellow-500 p-6 " variant="ghost" asChild>
           <Link href="/dashboard">
            Sign In
           </Link>
         </Button>
-        <Button className="bg-blue-500 p-6 w-1/4" variant="ghost" asChild>
+        <Button className="bg-blue-500 p-6  " variant="ghost" asChild>
           <Link href="/docs">
           Documentation
           </Link>
